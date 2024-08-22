cef790c133085966ce4b060aa3e512c7bdc46fb7
Dani-dzoe <danieldzotepe568@gmail.com>
2024-08-22 20:26:14 +0000

    task 1...6

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- A SQL script that creates a table users with the ff fields:
+-- id, email, name, country
+
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'IN') DEFAULT 'US' NOT NULL,
+);
